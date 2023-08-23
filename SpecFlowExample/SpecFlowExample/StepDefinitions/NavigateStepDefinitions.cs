7d317e1b0f20fd92713f7f87ee356c0960a9e3e3
Rowan <rowan.lea92@gmail.com>
2023-08-24 00:24:24 +0100

    Fixed hardcoding

--- SpecFlowExample/SpecFlowExample/StepDefinitions/NavigateStepDefinitions.cs
+++ SpecFlowExample/SpecFlowExample/StepDefinitions/NavigateStepDefinitions.cs
@@ -6,34 +6,41 @@ namespace SpecFlowExample.StepDefinitions
     public class NavigateStepDefinitions
     {
         private readonly IPage _page;
+        private readonly ScenarioContext _scenarioContext;
 
-        public NavigateStepDefinitions(Hooks.Hooks hooks)
+        public NavigateStepDefinitions(Hooks.Hooks hooks, ScenarioContext scenarioContext)
         {
             _page = hooks.Page;
+            _scenarioContext = scenarioContext;
         }
 
         [Given(@"I start on '([^']*)'")]
         public async Task GivenIStartOn(string startUrl)
         {
             await _page.GotoAsync(startUrl);
+
+            // Skip cookie confirmation box
             await _page.Locator("#hs-eu-confirmation-button").ClickAsync();
         }
 
         [When(@"I mouse over '([^']*)'")]
         public async Task WhenIMouseOver(string mouseOverTargetText)
         {
-            await _page
+            var mainMenuItem = _page
                 .GetByRole(AriaRole.Listitem)
-                .Filter(new() { HasText = mouseOverTargetText })
-                .HoverAsync();
+                .Filter(new() { HasText = mouseOverTargetText });
+
+            _scenarioContext["mainMenuItem"] = mainMenuItem;
+
+            await mainMenuItem.HoverAsync();
         }
 
         [When(@"I click on '([^']*)'")]
         public async Task WhenIClickOn(string mouseClickTargetText)
         {
-            await _page
-                .GetByRole(AriaRole.Listitem)
-                .Filter(new() { HasText = "Insights" })
+            var mainMenuItem = _scenarioContext.Get<ILocator>("mainMenuItem");
+            
+            await mainMenuItem
                 .GetByRole(AriaRole.Link)
                 .Filter(new() { HasText = mouseClickTargetText })
                 .ClickAsync();
